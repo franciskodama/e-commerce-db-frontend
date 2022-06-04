f883eb75188ae750a440ee8c9cfd9b220d640b48
Kodama <fk@fkodama.com>
2022-06-04 15:34:11 -0400

    Commit to save last changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9007d4105613439e76c"; }
+/******/ 		__webpack_require__.h = function() { return "8eced58cf43239821598"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,5 +1,32 @@
+import { useMutation } from '@apollo/client';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+mutation CREATE_PRODUCT_MUTATION(
+  $name: String!
+  $description: String!
+  $price: Int!
+  $image: Upload
+) {
+  createProduct(data: {
+    name: $name
+    description: $description
+    price: $price
+    status: 'AVAILABLE'
+    photo: {
+      create: {
+        image: $image, altText: $name
+      }
+    }
+  }) {
+    id
+    price
+    description
+    name
+  }
+}
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -8,6 +35,13 @@ export default function CreateProduct() {
     price: 32132,
     description: 'These are the best shoes',
   });
+
+  const [createProduct, { data, error, loading }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
 
   return (
     <Form
